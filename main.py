7ea66ab42803c2b87f9d315320ffb9b700831219
Yooondooo <kozyakovdanil04@gmail.com>
2025-10-02 23:17:23 +0300

    test user api

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Task Tracking Service", version="1.0.0")
 app.include_router(users.router)
 
+
 @app.get("/")
 def read_root():
     return {"message": "Task Tracking Service API"}
