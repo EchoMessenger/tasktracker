4747e1d72126abde77b77e4ee6e9055b262b4369
Yooondooo <magistryodo27@gmail.com>
2026-03-05 18:20:10 +0300

    helm + config database

--- database.py
+++ database.py
@@ -2,10 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:1363@localhost:5432/tasktracker"
-)
+DB_USER = os.getenv("DATABASE_USERNAME", "postgres")
+DB_PASSWORD = os.getenv("DATABASE_PASSWORD", "1363")
+DB_HOST = os.getenv("DATABASE_HOST", "localhost")
+DB_PORT = os.getenv("DATABASE_PORT", "5432")
+DB_NAME = os.getenv("DATABASE_NAME", "tasktracker")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
