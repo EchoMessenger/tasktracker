cb7cf4ce2f1cd3f44b710a92b37a9c8f947b4cf9
Yooondooo <magistryodo27@gmail.com>
2026-03-09 11:14:46 +0300

    repair intergration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,7 @@
 # tests/integration/conftest.py
 import pytest
 from sqlalchemy.orm import Session
-
+from database import get_db
 from main import app
 from crud import create_user
 from models.user import UserRole
@@ -20,9 +20,27 @@ def sample_user(db_session: Session):
     return create_user(db_session, user_data)
 
 
+# @pytest.fixture(autouse=True)
+# def override_current_user(sample_user):
+#     """Подменяет get_current_user — только для интеграционных тестов"""
+#     app.dependency_overrides[get_current_user] = lambda: sample_user.id
+#     yield
+#     app.dependency_overrides.pop(get_current_user, None)
+
 @pytest.fixture(autouse=True)
-def override_current_user(sample_user):
-    """Подменяет get_current_user — только для интеграционных тестов"""
+def override_dependencies(sample_user, db_session):
+    """Подменяет get_current_user И get_db для интеграционных тестов"""
+
+    # Подменяем get_db чтобы app использовал тестовую SQLite сессию
+    def override_get_db():
+        try:
+            yield db_session
+        finally:
+            pass  # не закрываем — это делает фикстура db_session
+
+    app.dependency_overrides[get_db] = override_get_db
     app.dependency_overrides[get_current_user] = lambda: sample_user.id
+
     yield
-    app.dependency_overrides.pop(get_current_user, None)
+
+    app.dependency_overrides.clear()
